6e8d02bbbf4cda206118d950ce9aaeffd1d57509
mlysaght <michael.a.lysaght@gmail.com>
2022-10-13 09:30:35 +0100

    Fix nat gateway routing

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "aws_eip" "nat-b" {
 
 resource "aws_nat_gateway" "nat-gw-a" {
   allocation_id = aws_eip.nat-a.id
-  subnet_id     = aws_subnet.private-subnet-a.id
+  subnet_id     = aws_subnet.public-subnet-a.id
   depends_on = [
     aws_internet_gateway.igw
   ]
@@ -137,7 +137,7 @@ resource "aws_nat_gateway" "nat-gw-a" {
 
 resource "aws_nat_gateway" "nat-gw-b" {
   allocation_id = aws_eip.nat-b.id
-  subnet_id     = aws_subnet.private-subnet-b.id
+  subnet_id     = aws_subnet.public-subnet-b.id
   depends_on = [
     aws_internet_gateway.igw
   ]
